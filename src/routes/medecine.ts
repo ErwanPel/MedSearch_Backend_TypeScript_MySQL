ec7ceab80ab1b7d28f26151eb4ba96c40206fca7
Erwan <erwanpellerinpro@gmail.com>
2023-12-04 12:07:24 +0100

    add put root for add and remove quantity and create filter for research

--- src/routes/medecine.ts
+++ src/routes/medecine.ts
@@ -14,15 +14,30 @@ type TDataPost = {
   quantity: number;
 };
 
+type TDataPutQuantity = {
+  id: number;
+  quantity: number;
+};
+
 medRouter.get("/medecine", (req: Request, res: Response) => {
   try {
-    connection.query(
-      "SELECT * FROM medecine",
-      function (err: any, data: TDataGetAll) {
-        if (err) console.log(err);
-        else res.status(200).json(data);
-      }
-    );
+    if (req.query.search) {
+      connection.query(
+        ` SELECT UPPER(medecine.name) AS medecine, family_med.family, medecine.quantity, supply.name as supply FROM medecine_supply LEFT JOIN medecine ON medecine.id = medecine_supply.medecine_id JOIN supply ON supply.id = medecine_supply.supply_id JOIN family_med ON family_med.id = medecine.family_id WHERE medecine.name LIKE "%${req.query.search}%" ORDER BY medecine.name ASC`,
+        function (err: any, data: TDataGetAll) {
+          if (err) console.log(err);
+          else res.status(200).json(data);
+        }
+      );
+    } else {
+      connection.query(
+        "SELECT UPPER(medecine.name) AS medecine, family_med.family, medecine.quantity, supply.name as supply FROM medecine_supply LEFT JOIN medecine ON medecine.id = medecine_supply.medecine_id JOIN supply ON supply.id = medecine_supply.supply_id JOIN family_med ON family_med.id = medecine.family_id ORDER BY medecine.name ASC",
+        function (err: any, data: TDataGetAll) {
+          if (err) console.log(err);
+          else res.status(200).json(data);
+        }
+      );
+    }
   } catch (error) {
     res.status(500).json({ message: "an error occured in /medecine" });
   }
@@ -31,15 +46,17 @@ medRouter.get("/medecine", (req: Request, res: Response) => {
 medRouter.post("/medecine", (req: Request, res: Response) => {
   try {
     console.log(req.body);
-    const { name, quantity } = req.body;
+    const { name, quantity, family_id } = req.body;
     if (
       name &&
       typeof name === "string" &&
       quantity &&
-      typeof quantity === "number"
+      typeof quantity === "number" &&
+      family_id &&
+      typeof family_id === "number"
     ) {
       connection.query(
-        `INSERT INTO medecine (name, quantity) VALUES ('${name}', ${quantity});`,
+        `INSERT INTO medecine (name, quantity, family_id) VALUES ('${name}', ${quantity}, ${family_id});`,
         function (err: any, data: TDataPost) {
           if (err) {
             console.log(err);
@@ -57,6 +74,52 @@ medRouter.post("/medecine", (req: Request, res: Response) => {
       );
     } else {
       throw { status: 400, message: "missing name for medecine" };
+    }
+  } catch (error) {
+    res.status(500).json({ message: "an error occured in /medecine" });
+  }
+});
+
+medRouter.put("/medecine/quantity/add", (req: Request, res: Response) => {
+  const { id, quantity } = req.body;
+  try {
+    if (id && quantity) {
+      connection.query(
+        `UPDATE medecine SET quantity = quantity + ${quantity} WHERE id = ${id}`,
+        function (err: any, data: TDataPutQuantity) {
+          if (err) {
+            console.log(err);
+          } else
+            res.status(200).json({
+              message: `quantité ajouté`,
+            });
+        }
+      );
+    } else {
+      throw { status: 400, message: "missing quantity or id for medecine" };
+    }
+  } catch (error) {
+    res.status(500).json({ message: "an error occured in /medecine" });
+  }
+});
+
+medRouter.put("/medecine/quantity/remove", (req: Request, res: Response) => {
+  const { id, quantity } = req.body;
+  try {
+    if (id && quantity) {
+      connection.query(
+        `UPDATE medecine SET quantity = quantity - ${quantity} WHERE id = ${id}`,
+        function (err: any, data: TDataPutQuantity) {
+          if (err) {
+            console.log(err);
+          } else
+            res.status(200).json({
+              message: `quantité supprimé`,
+            });
+        }
+      );
+    } else {
+      throw { status: 400, message: "missing quantity or id for medecine" };
     }
   } catch (error) {
     res.status(500).json({ message: "an error occured in /medecine" });
